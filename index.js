bab9df5deb9bc3117a1eb88d04c39002b2e197a0
YuriLohan <yurilohan1000@gmail.com>
2024-12-31 13:07:31 -0300

    classes de jogo v1

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+iniciarJogo()
 class jogo{
+    constructor(nomeHeroi,nivelHeroi,tipoHeroi){
+        this.nomeHeroi = nomeHeroi
+        this.nivelHeroi = nivelHeroi
+        this.tipoHeroi = tipoHeroi.toLowerCase()
+    }
+    ataques(){
+        let ataquesPorTipo = {
+            guerreiro: "espada",
+            mago: "magia",
+            monge: "artes marciais",
+            ninja: "shuriken",
 
+        }
+        return ataquesPorTipo[this.tipoHeroi] || "ataque básico"
+    }
+    saida(){
+        console.log(`O ${this.tipoHeroi} de nome ${this.nomeHeroi} do nível ${this.nivelHeroi} atacou bravamente usando ${this.ataques()}`)
+    }
 }
-function ataques(){
+async function coletarDadosHeroi(){
+    let nome = await perguntar("Qual é o nome do seu herói? ")
+    let nivel = await perguntar("Qual é o nível do seu herói? ")
+    let tipo = await perguntar("Qual é o tipo do seu herói?(ex: guerreiro,mago, monge, ninja) ")
 
+    let heroi = new jogo(nome,nivel, tipo)
+    return heroi
 }
-function saida(){
-
+function perguntar(pergunta) {
+    return new Promise((resolve) => {
+        rl.question(pergunta, (resposta) => {
+            resolve(resposta);
+        });
+    });
+}
+async function iniciarJogo() {
+    let heroi = await coletarDadosHeroi();
+    heroi.saida(); 
+    rl.close();  
 }
